833cb3d8e2f539cce9825bab0dcd4be2c6e8d2da
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2023-04-19 12:38:44 -0700

    move left added

--- Running Game/Assets/Script/PlayerControllerScript.cs
+++ Running Game/Assets/Script/PlayerControllerScript.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerControllerScript : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpforce = 10;
+    public float gravityModifier = 1;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        //playerRb.AddForce(Vector3.up * 500);
+        Physics.gravity *= gravityModifier;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 500);
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
---
+++ Running Game/Assets/Script/SpawnManagerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject obstaclePrefab;
+    public Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
